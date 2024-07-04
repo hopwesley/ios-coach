cb9f2b2f46e8da3a31de71b63da312f6f8843d27
basdevelop <hyperorchid@outlook.com>
2024-07-04 14:23:17 +0800

    save tmp

--- controller/VideoCompare.swift
+++ controller/VideoCompare.swift
@@ -8,7 +8,7 @@ import Foundation
 
 import AVFoundation
 import CoreImage
- 
+
 class VideoCompare: ObservableObject {
         @Published var processingMessage: String = "开始处理..."
         var videoWidth:Int = 0
@@ -29,27 +29,10 @@ class VideoCompare: ObservableObject {
                 self.assetA = AVAsset(url: videoA)
                 self.assetB = AVAsset(url: videoB)
                 
+                logProcessInfo("初始化GPU")
                 try initGpuDevice()
                 try await self.prepareParametersFromeOneVideo()
-        }
-        
-        private func prepareParametersFromeOneVideo() async throws{
-                guard let videoTrack = try await self.assetA.loadTracks(withMediaType: .video).first else {
-                        throw ASError.noValidVideoTrack
-                }
-                
-                let videoSize = try await videoTrack.load(.naturalSize)
-                self.videoWidth = Int(videoSize.width)
-                self.videoHeight = Int(videoSize.height)
-                self.pixelSize = self.videoWidth * self.videoHeight
-                
-                self.textureDescriptor = MTLTextureDescriptor.texture2DDescriptor(
-                        pixelFormat: .rgba8Unorm,
-                        width: self.videoWidth,
-                        height: self.videoHeight,
-                        mipmapped: false
-                )
-                textureDescriptor.usage = [.shaderRead, .shaderWrite]
+                try await  self.processVideo()
         }
         
         func initGpuDevice() throws{
@@ -76,30 +59,44 @@ class VideoCompare: ObservableObject {
                 blockHistogramPipe = try device.makeComputePipelineState(function: quantizeGradientFun)
         }
         
-        private func iterateVideoFrame(asset:AVAsset,callBack: ((MTLTexture) -> Void)?) async throws{
-                let reader = try AVAssetReader(asset: asset)
-                guard let videoTrack = try await asset.loadTracks(withMediaType: .video).first else {
+        private func prepareParametersFromeOneVideo() async throws{
+                guard let videoTrack = try await self.assetA.loadTracks(withMediaType: .video).first else {
                         throw ASError.noValidVideoTrack
                 }
                 
-                let trackReaderOutput = AVAssetReaderTrackOutput(track: videoTrack, outputSettings: [
-                        (kCVPixelBufferPixelFormatTypeKey as String): Int(kCVPixelFormatType_32BGRA)
-                ])
-                reader.add(trackReaderOutput)
-                reader.startReading()
+                let videoSize = try await videoTrack.load(.naturalSize)
+                self.videoWidth = Int(videoSize.width)
+                self.videoHeight = Int(videoSize.height)
+                self.pixelSize = self.videoWidth * self.videoHeight
                 
-                while let sampleBuffer = trackReaderOutput.copyNextSampleBuffer() {
-                        guard  let frame = pixelBufferToTexture(sampleBuffer) else{
-                                throw ASError.readVideoDataFailed
-                        }
-                        if let callBack = callBack {
-                                callBack(frame)
-                        }
-                }
-                reader.cancelReading()
+                self.textureDescriptor = MTLTextureDescriptor.texture2DDescriptor(
+                        pixelFormat: .rgba8Unorm,
+                        width: self.videoWidth,
+                        height: self.videoHeight,
+                        mipmapped: false
+                )
+                textureDescriptor.usage = [.shaderRead, .shaderWrite]
         }
         
+        private func prepareDeviceBuffer(){
+                
+        }
+        
+        private func processVideo() async throws{
+                var counter = 0;
+                try await iterateVideoFrame(){frameA, frameB in
+                        counter+=1
+                        self.logProcessInfo("处理第\(counter)帧")
+                }
+        }
+        
+}
+
+
+extension  VideoCompare{
+        
         private func pixelBufferToTexture(_ sbuf: CMSampleBuffer)->MTLTexture?{
+                
                 guard let videoFrame = CMSampleBufferGetImageBuffer(sbuf) else{
                         return nil
                 }
@@ -113,5 +110,42 @@ class VideoCompare: ObservableObject {
                 context.render(ciImage, to: inputTexture, commandBuffer: nil, bounds: ciImage.extent, colorSpace: CGColorSpaceCreateDeviceRGB())
                 
                 return inputTexture
+        }
+        
+        private func iterateVideoFrame(callBack: ((MTLTexture,MTLTexture) -> Void)?) async throws{
+                let readerA = try AVAssetReader(asset: self.assetA)
+                let readerB = try AVAssetReader(asset: self.assetA)
+                guard let videoTrackA = try await self.assetA.loadTracks(withMediaType: .video).first,
+                let videoTrackB = try await self.assetB.loadTracks(withMediaType: .video).first else {
+                        throw ASError.noValidVideoTrack
+                }
+                
+                let trackReaderOutputA = AVAssetReaderTrackOutput(track: videoTrackA, outputSettings: [
+                        (kCVPixelBufferPixelFormatTypeKey as String): Int(kCVPixelFormatType_32BGRA)
+                ])
+                let trackReaderOutputB = AVAssetReaderTrackOutput(track: videoTrackB, outputSettings: [
+                        (kCVPixelBufferPixelFormatTypeKey as String): Int(kCVPixelFormatType_32BGRA)
+                ])
+                readerA.add(trackReaderOutputA)
+                readerB.add(trackReaderOutputB)
+                readerA.startReading()
+                readerB.startReading()
+                
+                while let sampleBufferA = trackReaderOutputA.copyNextSampleBuffer(),
+                      let sampleBufferB = trackReaderOutputB.copyNextSampleBuffer(){
+                        guard  let frameA = pixelBufferToTexture(sampleBufferA),
+                               let frameB = pixelBufferToTexture(sampleBufferB)else{
+                                throw ASError.readVideoDataFailed
+                        }
+                        if let callBack = callBack {
+                                callBack(frameA,frameB)
+                        }
+                }
+                readerA.cancelReading()
+                readerB.cancelReading()
+        }
+        
+        private func logProcessInfo(_ info:String){
+                DispatchQueue.main.async { self.processingMessage = info}
         }
 }
